4673217e4061bbaa7e252fe8474a4494db7ca6bb
cl2112 <cl2112cl2112@gmail.com>
2017-04-12 13:34:27 -0400

    retrived and displayed the items for sale.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,27 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+
+// console.log("hello!");
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'SomebodysOutThere42',
+  database : 'bamazon'
+});
+
+connection.connect();
+ 
+connection.query('SELECT item_id, product_name, price FROM products', function (error, results, fields) {
+  if (error) throw error;
+  // console.log('The solution is: ', results);
+  for (var i = 0; i < results.length; i++) {
+  	console.log("---------------------------------------------------------------");
+  	console.log("Product Name: " + results[i].product_name);
+  	console.log("Price: " + results[i].price);
+  	console.log("Product ID: " + results[i].item_id);
+  	console.log("================================================================");
+  };
+});
+ 
+connection.end();
