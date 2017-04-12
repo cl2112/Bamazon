b78dfe988a6af78127e2ff24b7b9a933e52f5012
cl2112 <cl2112cl2112@gmail.com>
2017-04-12 13:42:39 -0400

    started working on the inquirer prompts.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,12 +16,34 @@ connection.query('SELECT item_id, product_name, price FROM products', function (
   if (error) throw error;
   // console.log('The solution is: ', results);
   for (var i = 0; i < results.length; i++) {
-  	console.log("---------------------------------------------------------------");
+  	console.log("----------------------------------------------------------------");
   	console.log("Product Name: " + results[i].product_name);
   	console.log("Price: " + results[i].price);
   	console.log("Product ID: " + results[i].item_id);
   	console.log("================================================================");
   };
+});
+
+inquirer.prompt([
+
+	{
+		type: "input",
+		message: "What is the ID of the item you would like to buy?",
+		name: "ID"
+	},	
+
+	{
+		type: "input",
+		message: "How many would you like to buy?",
+		name: "quantity"
+	}
+
+]).then(function(answers){
+
+	console.log(answers);
+	console.log(answers.ID);
+	console.log(answers.quantity);
+
 });
  
 connection.end();
