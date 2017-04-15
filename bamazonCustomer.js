49a4108958cfb739f363ba57d97d02686b9252e5
cl2112 <cl2112cl2112@gmail.com>
2017-04-15 16:13:07 -0400

    started working on the function to handle the database update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,11 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
+
+function updateDatabase() {
+	connection.query('Update');
+};
+
 // console.log("hello!");
 
 var connection = mysql.createConnection({
@@ -9,6 +14,12 @@ var connection = mysql.createConnection({
   password : 'SomebodysOutThere42',
   database : 'bamazon'
 });
+
+
+function buyItem(itemID, stock, quantity){
+	connection.query('UPDATE products SET stock_quantity = ? WHERE item_id = ?')
+}
+
 
 connection.connect();
  
@@ -43,12 +54,14 @@ connection.query('SELECT * FROM products', function (error, results, fields) {
 
 		var itemID = answers.ID;
 		var quantity = answers.quantity;
+		var stock = results[itemID-1].stock_quantity;
 
-		if (results[itemID-1].stock_quantity < quantity){
+		if (stock < quantity){
 			console.log("Insufficient quantity!");
-		} else if (results[itemID-1].stock_quantity >= quantity) {
+		} else if (stock >= quantity) {
 			console.log("Right away sir!");
-			
+			buyItem(itemID, stock, quantity);
+
 		} else {
 			console.log("There is some problem with the quantity check!");
 		}
